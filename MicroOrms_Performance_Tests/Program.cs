24865eb2b6942ec73c8f372c991258e68bac9cf7
Oleksandr Dudik <oleksandr.dudik@sigma.software.com>
2016-11-13 15:45:15 +0200

    Add project files.

---
+++ MicroOrms_Performance_Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroOrms_Performance_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
