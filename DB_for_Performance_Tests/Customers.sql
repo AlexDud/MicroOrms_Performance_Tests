6d5dbedbed6df2c11445016f3f8a7e3a6f455553
Oleksandr Dudik <oleksandr.dudik@sigma.software.com>
2016-11-16 13:56:42 +0200

    Add DB project

---
+++ DB_for_Performance_Tests/Customers.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[CustomerId] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Surname] [nvarchar](50) NOT NULL,
+	[Email] [nvarchar](150) NULL,
+	[Phone] [nvarchar](50) NULL,
+	[Age] [int] NULL,
+	[DateOfBirth] [datetime2](7) NULL,
+	[CustomerStatusId] [int] NULL, 
+    CONSTRAINT [PK_Customers] PRIMARY KEY ([CustomerId]),
+)
